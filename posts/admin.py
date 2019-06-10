e917c04b3e4756fcf98167682343b5a1a8c3c3b1
Howard Shim <howardshim@gmail.com>
2019-06-10 15:16:12 -0700

    initial commit

---
+++ posts/admin.py
@@ -0,0 +1,11 @@
+# posts/admin.py
+
+from django.contrib import admin
+
+from .models import Post
+
+
+admin.site.register(Post)
+
+
+# Register your models here.
---
+++ posts/models.py
@@ -0,0 +1,13 @@
+# posts/models.py
+
+from django.db import models
+
+
+class Post(models.Model):
+    text = models.TextField()
+
+    def __str__(self):
+        return self.text[:50]
+
+    
+# Create your models here.
